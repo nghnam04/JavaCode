d15e4fd8d6048e5842cc1dac0b7612740b9ed29e
Nguyen Hoang Nam <hoangnam2004hp@gmail.com>
2024-04-15 23:56:59 +0700

    Test phương thức khởi tọa tự viết

---
+++ TestPTKT2.java
@@ -0,0 +1,14 @@
+//PT khởi tạo mặc định tự viết
+public class BackAccount{
+    private String owner;
+    private double balance;
+    public BankAccount(){
+    owner = "noname";
+    }
+    }
+    
+public class TestPTKT2 {
+    public static void main(String args[]){
+        BankAccount acc1 = new BankAccount();
+        }
+}
