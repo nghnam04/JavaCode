20f8c20de899bacace468fd97fcc5db96025f8ef
Nguyen Hoang Nam <hoangnam2004hp@gmail.com>
2024-04-15 23:17:10 +0700

    Test phương thức khởi tạo mặc định do Java cung cấp

---
+++ TestPTKT.java
@@ -0,0 +1,10 @@
+ class BankAccount{
+    private String owner;
+    private double balance;
+    }
+    
+public class TestPTKT {
+    public static void main(String args[]){
+        BankAccount acc1 = new BankAccount();
+        }
+}
